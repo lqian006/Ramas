f5716f85941b3adc9071d2b6ed974036e8463e42
lqian006 <lqian006@gmail.com>
2025-05-29 03:32:11 +0200

    Versión 4 falta

--- Version1/segment.py
+++
@@ -1,8 +0,0 @@
-import node
-class Segment:
-    def __init__(self, name: str, origin: node, destination: node):
-        self.name=name
-        self.origin= origin
-        self.destination = destination
-        self.cost = node.Distance(origin, destination)
-
